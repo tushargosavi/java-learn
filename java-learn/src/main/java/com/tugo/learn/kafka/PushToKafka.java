3042430f772fb653c5a6a0d9fe58e9501a69dd28
Tushar R. Gosavi <tushargosavi@gmail.com>
2019-07-06 18:56:02 +0530

    Simple kafka producer example

--- java-learn/src/main/java/com/tugo/learn/kafka/PushToKafka.java
+++ java-learn/src/main/java/com/tugo/learn/kafka/PushToKafka.java
@@ -2,9 +2,12 @@ package com.tugo.learn.kafka;
 
 import java.util.Properties;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 public class PushToKafka
 {
-  Properties getProducerProperties()
+  static Properties getProducerProperties()
   {
     Properties props = new Properties();
     props.put("bootstrap.servers", "localhost:9092");
@@ -12,6 +15,17 @@ public class PushToKafka
     props.put("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
     props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
     return props;
+  }
+
+  public static void main(String[] args)
+  {
+    Properties props = getProducerProperties();
+    KafkaProducer<byte[], byte[]> kp = new KafkaProducer<>(props);
+
+    ProducerRecord<byte[], byte[]> pr = new ProducerRecord<>("test1", new byte[20], new byte[30]);
+    kp.send(pr);
+    kp.flush();
+    kp.close();
   }
 
 }
